c3162a4e28aae3d8a96a17a9f77817871d1d4c16
Gabriel Godinho dos Santos <gabriel234417@gmail.com>
2022-08-18 13:16:16 -0300

    Added more operations
    Foram adicionadas as operações de seno, cosseno e tangente e o valor do número de Euler

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,15 +1,20 @@
+// Adiciona o caractere do botão na caixa de texto
 function character(num) {
     
     let box = document.getElementById("calc")
     box.value += num
+
 }
 
+// Limpar a caixa de texto
 function clearBox() {
     
     let box = document.getElementById("calc")
     box.value = null
+
 }
 
+// Resultado
 function getResult() {
     
     let box = document.getElementById("calc")
@@ -20,14 +25,18 @@ function getResult() {
     } else {
         box.value = "Erro!"
     }
+
 }
 
+// Valor de Pi
 function piValue() {
     
     let box = document.getElementById("calc")
-    box.value += 3.14159265358979
+    box.value += Math.PI
+
 }
 
+// Raiz quadrada
 function squareRoot() {
     
     let box = document.getElementById("calc")
@@ -36,11 +45,12 @@ function squareRoot() {
     if (eval(expression) >= 0) {
         box.value = Math.sqrt(eval(expression))
     } else {
-        box.value = "Imaginary Number!"
+        box.value = "Número Imaginário!"
     }
     
 }
 
+// Fatorial
 function fat() {
     
     let box = document.getElementById("calc")
@@ -51,4 +61,39 @@ function fat() {
     }
 
     box.value = times
+
+}
+
+function sinValue() {
+
+    let box = document.getElementById("calc")
+    let expression = box.value
+
+    box.value = Math.sin(eval(expression))
+
+}
+
+function cosValue() {
+
+    let box = document.getElementById("calc")
+    let expression = box.value
+
+    box.value = Math.cos(eval(expression))
+
+}
+
+function tanValue() {
+
+    let box = document.getElementById("calc")
+    let expression = box.value
+
+    box.value = Math.tan(eval(expression))
+
+}
+
+function eulerValue() {
+
+    let box = document.getElementById("calc")
+    box.value = Math.E
+
 }
